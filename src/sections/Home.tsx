214b0d21d9c269b2ab1df83411e78c43d552b0b6
Brandon Eduardo Merchan sandoval <merchito@Brandons-MacBook-Pro.local>
2026-01-07 21:39:01 -0500

    arreglopagina

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -21,7 +21,7 @@ const Home = () => {
           align-items: center;
           text-align: center;
           width: 100vw;
-          height: 750px;
+          height: 55vw;
           margin-left: calc(-50vw + 50%);
           position: relative;
           z-index: 1;
